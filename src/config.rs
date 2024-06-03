92fa07ee3124344903dbe92942feb877cbbfa8a5
obaraelijah <elijahobara357@gmail.com>
2024-06-03 11:20:04 +0300

    feat: Pr status logic enum

--- src/config.rs
+++ src/config.rs
@@ -13,6 +13,22 @@ pub struct Config {
     pub default_repo: Rc<str>,
 }
 
+pub(crate) enum PRStatus {
+    Open,
+    Closed,
+    All,
+}
+
+impl Into<String> for &PRStatus {
+    fn into(self) -> String {
+        match self {
+            PRStatus::Open => "open".to_string(),
+            PRStatus::Closed => "closed".to_string(),
+            PRStatus::All => "all".to_string(),
+        }
+    }
+}
+
 pub async fn query(def_owner: Rc<str>, defo_repo: Rc<str>) {
     let default = Repository {
         owner: def_owner.to_string(),
