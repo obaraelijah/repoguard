1e2562caebb05173522f49e0f65e75fef40b4b46
obaraelijah <elijahobara357@gmail.com>
2024-06-03 14:24:07 +0300

    feat: Serving prometheus metrics via hyper

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use clap::Parser;
 use config::Config;
 use jsonwebtoken::EncodingKey;
 use octocrab::{models::AppId, Octocrab};
+use prometheus::serve;
 
 use log::{debug, info, trace};
 
@@ -14,7 +15,11 @@ mod args;
 mod config;
 mod prometheus;
 
-fn main() {
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // setup logging
+
+    // setup octo client
     let args = Args::parse();
     simple_logger::init_with_level(args.log_level.into()).unwrap();
 
@@ -32,21 +37,34 @@ fn main() {
         trace!("Using personal access token");
         octo_builder = octo_builder.personal_token(pat);
     } else {
+        trace!("Using app id and secret");
         let app_id = args.app_id.expect("Either App id or PAT are required");
         let app_secret = args.app_secret.unwrap();
         octo_builder = octo_builder.app(
-            AppId(app_id.parse().unwrap()), 
+            AppId(app_id.parse().unwrap()),
             EncodingKey::from_base64_secret(&app_secret).unwrap(),
         );
     }
     let octo = Arc::new(octo_builder.build().expect("Failed to build octocrab"));
 
-    for monitor in &config.monitoring {
-        query(
-            &octo, 
-            config.default_owner.clone(), 
-            config.default_repo.clone(), 
-            monitor.clone()
-        );    
+    tokio::spawn(async {
+        serve().await.expect("Failed to start prometheus server");
+    });
+
+    info!("Starting the monitoring loop");
+    let period = std::time::Duration::from_secs(config.monitor_period);
+    debug!("Monitoring period: {:?}", period);
+    let mut interval = tokio::time::interval(period);
+    loop {
+        interval.tick().await;
+        for monitor in &config.monitoring {
+            query(
+                &octo,
+                config.default_owner.clone(),
+                config.default_repo.clone(),
+                monitor.clone(),
+            )
+            .await;
+        }
     }
 }
